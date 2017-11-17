c3e5c3342db7dc3ade6c55a70490bb74be3a029e
Gil Shoshan <shoshan.gil@gmail.com>
2017-11-17 21:52:24 +0200

    add Firebase upload 1

--- app/src/main/java/com/realtimehitchhiker/hitchgo/MainActivity.java
+++ app/src/main/java/com/realtimehitchhiker/hitchgo/MainActivity.java
@@ -8,8 +8,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
@@ -21,23 +24,30 @@ import android.test.mock.MockPackageManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.ProfilePictureView;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.ResultCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.IgnoreExtraProperties;
 
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -45,31 +55,33 @@ public class MainActivity extends AppCompatActivity {
     //public static final String EXTRA_MESSAGE = "com.realtimehitchhiker.hitchgo.MESSAGE"; for intent //intent.putExtra(EXTRA_MESSAGE, message); to be unique
     private static final int PERMISSION_LOCATION_REQUEST_CODE = 2;
     private static final int RC_SIGN_IN = 123; //FireBase
-    public static final String TAG = "MAIN";
+    public static final String TAG = "MAIN_DEBUG";
 
     //FireBase
     private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
     private FirebaseDatabase database;
+
+    private String facebookUserId = null;
+    private String photoUrl = null;
 
     // Choose authentication providers
     List<AuthUI.IdpConfig> authProviders = Arrays.asList(
-            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-            new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
-            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+            //new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+            //new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+            //new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
             new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()//, //to add more permission than the default, add ".setPermissions(Arrays.asList("user_friends"))" before ".build()"
             //new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()
     );
 
-    private Button btnShowLocation;
     private Button btnLog;
-    private TextView txtShowLocation;
-    private BroadcastReceiver broadcastReceiverLocUpdate;
-    private BroadcastReceiver broadcastReceiverLocOff;
+    private TextView txtShowLocation, txtWelcome;
+    private ImageView imProfile;
+    private BroadcastReceiver broadcastReceiverLocUpdate, broadcastReceiverLocOff;
     private Location location = null;
 
     //flag
-    int flag_service = 0;
-    int flag_login = 0;
+    private int flag_login = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,11 +94,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //For FireBase
         mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
         database = FirebaseDatabase.getInstance();
 
-        btnShowLocation = (Button) findViewById(R.id.button_testCoordinates);
         txtShowLocation = (TextView) findViewById(R.id.textView_testCoordinates);
+        txtWelcome = (TextView) findViewById(R.id.textView_welcome_profile);
         btnLog = (Button) findViewById(R.id.button_login);
+        imProfile = (ImageView) findViewById(R.id.profile_image);
+        //imProfile.setMaxHeight(100);
+        //imProfile.setMaxWidth(100);
+        //imProfile.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imProfile.setCropToPadding(true);
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        updateUI(currentUser);
     }
 
     @Override
@@ -97,17 +118,6 @@ public class MainActivity extends AppCompatActivity {
         // Check permissions and start service location
         if(!runtimePermissions())
             enableLocationService();
-
-        enableLogButtton();
-
-        //For FireBase
-        // Check if user is signed in (non-null) and update UI accordingly.
-        if (mAuth.getCurrentUser() != null) {
-            // already signed in
-            //updateUI(currentUser);
-        } else {
-            // not signed in
-        }
 
     }
 
@@ -133,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onReceive(Context context, Intent intent) {
                     Log.d(LocationService.TAG, LocationService.BROADCAST_ACTION_LOC_OFF );
                     if(isAllActiveProviderDisabled())
-                        showSettingsAlert();
+                        showLocationSettingsAlert();
                 }
             };
         }
@@ -163,6 +173,9 @@ public class MainActivity extends AppCompatActivity {
         if(broadcastReceiverLocOff != null){
             unregisterReceiver(broadcastReceiverLocOff);
         }
+
+        Intent i_stop = new Intent(getApplicationContext(), LocationService.class);
+        stopService(i_stop);
     }
 
     @Override
@@ -189,8 +202,20 @@ public class MainActivity extends AppCompatActivity {
             }
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Log.d(TAG, "Successfully signed in = " + user.toString());
+                //FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                currentUser = mAuth.getCurrentUser();
+                updateUI(currentUser);
+                Log.d(TAG, "Successfully signed in = " + currentUser.toString());
+                Log.d(TAG, "Successfully signed in = " + currentUser.getDisplayName());////////////////////////////////
+                Log.d(TAG, "Successfully signed in = " + currentUser.getEmail());////////////////////////////////
+                Log.d(TAG, "Successfully signed in = " + currentUser.getPhoneNumber());////////////////////////////////
+                Log.d(TAG, "Successfully signed in = " + currentUser.getProviderId());
+                Log.d(TAG, "Successfully signed in = " + currentUser.getProviders());
+                Log.d(TAG, "Successfully signed in = " + currentUser.getProviderData());
+                Log.d(TAG, "Successfully signed in = " + currentUser.getUid());
+                Log.d(TAG, "Successfully signed in = " + currentUser.getIdToken(true));
+                Log.d(TAG, "Successfully signed in = " + currentUser.getIdToken(false));
+                Log.d(TAG, "Successfully signed in = " + currentUser.getPhotoUrl());////////////////////////////////
                 //startActivity(SignedInActivity.createIntent(this, response));
                 //finish();
                 //return;
@@ -238,18 +263,30 @@ public class MainActivity extends AppCompatActivity {
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     public void onComplete(@NonNull Task<Void> task) {
                         // user is now signed out
+                        currentUser = mAuth.getCurrentUser(); //normally =null
+                        facebookUserId = null;
+                        photoUrl = null;
+                        updateUI(currentUser);
                     }
                 });
     }
 
-    private void enableLogButtton() {
+    private void enableLogButtton(boolean bool) {
+        if (bool){ //already signed in
+            flag_login = 1;
+            btnLog.setText(R.string.button_logout);
+        }
+        else {
+            flag_login = 0;
+            btnLog.setText(R.string.button_login);
+        }
         btnLog.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 if (flag_login == 0) {
                     Button b = (Button) view;
                     b.setText(R.string.button_logout);
-                    flag_login++;
+                    flag_login = 1;
                     logInAuth(view);
 
                 } else {
@@ -263,30 +300,71 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void enableLocationService() {
-
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (flag_service == 0) {
-                    Button b = (Button) view;
-                    b.setText("STOP LOC SERVICE");
-                    flag_service++;
-                    Toast.makeText(getApplicationContext(), "Location Service ON",
-                            Toast.LENGTH_SHORT).show();
-                    Intent i_start = new Intent(getApplicationContext(), LocationService.class);
-                    startService(i_start);
-                } else {
-                    flag_service = 0;
-                    Button b = (Button) view;
-                    b.setText("START LOC SERVICE");
-                    Toast.makeText(getApplicationContext(), "Location Service OFF",
-                            Toast.LENGTH_SHORT).show();
-                    Intent i_stop = new Intent(getApplicationContext(), LocationService.class);
-                    stopService(i_stop);
+    private void updateUI(FirebaseUser user){
+        if(user!=null){
+            //already signed in
+            enableLogButtton(true);
+            txtWelcome.setText("Welcome back " + user.getDisplayName());
+            // find the Facebook profile and get the user's id
+            for(UserInfo profile : currentUser.getProviderData()) {
+                // check if the provider id matches "facebook.com"
+                if(FacebookAuthProvider.PROVIDER_ID.equals(profile.getProviderId())) {
+                    facebookUserId = profile.getUid();
                 }
             }
-        });
+            // construct the URL to the profile picture, with a custom height
+            // alternatively, use '?type=small|medium|large' instead of ?height=500
+            photoUrl = "https://graph.facebook.com/" + facebookUserId + "/picture?type=large";
+            new DownloadImageTask(imProfile).execute(photoUrl);
+        }
+        else{
+            enableLogButtton(false);
+            txtWelcome.setText("Welcome, please log in");
+            imProfile.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon = null;
+            InputStream in = null;
+            try {
+                in = new java.net.URL(urldisplay).openStream();
+                mIcon = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    }
+                    catch(IOException ioex) {
+                        //Very bad things just happened... handle it
+                        Log.wtf(TAG, "Stream.close throw exception :" + ioex );
+                    }
+                }
+            }
+            return mIcon;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
+    private void enableLocationService() {
+        Toast.makeText(getApplicationContext(), "Location Service ON",
+                Toast.LENGTH_SHORT).show();
+        Intent i_start = new Intent(getApplicationContext(), LocationService.class);
+        startService(i_start);
     }
 
     /**
@@ -357,7 +435,7 @@ public class MainActivity extends AppCompatActivity {
      * Function to show settings alert dialog
      * On pressing Settings button will launch Settings Options
      * */
-    public void showSettingsAlert(){
+    public void showLocationSettingsAlert(){
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
         // Setting Dialog Title
         alertDialog.setTitle("GPS is settings");
@@ -374,7 +452,7 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
-                //showSettingsAlert();
+                showLocationSettingsAlert();
             }
         });
         // Showing Alert Message
@@ -400,38 +478,25 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @IgnoreExtraProperties
-    public class User {
 
-        public String username;
-        //public String email;
-        public String lat;
-        public String lon;
-
-        public User() {
-            // Default constructor required for calls to DataSnapshot.getValue(User.class)
-            username = "Gil";
-            //lat=location.getLatitude();
-            //lon=location.getLongitude();
-            lat="0.0";
-            lon="1.0";
+    public void addRideToFirebase(View view) {
+        if(location==null){
+            Toast.makeText(getApplicationContext(), "Enable location or wait for location fix",
+                    Toast.LENGTH_SHORT).show();
+            return;
         }
-
-        //public User(String username) {
-          //  this.username = username;
-        //}
-
-    }
-    public void testFirebase(View view) {
-        //User user = new User();
+        if(currentUser==null){
+            Toast.makeText(getApplicationContext(), "You are not logged in\nLog in please",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
         Double lat=location.getLatitude();
         Double lon=location.getLongitude();
-        //DatabaseReference myRef = database.getReference();
-        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("users").child("1245678").child("name").setValue("Gil");
-        mDatabase.child("users").child("1245678").child("latitude").setValue(lat);
-        mDatabase.child("users").child("1245678").child("longtitude").setValue(lon);
-        //mDatabase.child("users").child("1245678").child("phoneNumber").setValue();
+        DatabaseReference myRef = database.getReference();
+
+        myRef.child("users").child(facebookUserId).child("name").setValue(currentUser.getDisplayName());
+        myRef.child("users").child(facebookUserId).child("latitude").setValue(lat);
+        myRef.child("users").child(facebookUserId).child("longitude").setValue(lon);
 
 
 
