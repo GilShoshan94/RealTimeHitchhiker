559cc55071e1228cba0925c720df26b067a01083
Gil Shoshan <shoshan.gil@gmail.com>
2017-11-19 01:13:47 +0200

    update preferences

--- app/src/main/java/com/realtimehitchhiker/hitchgo/SettingsActivity.java
+++ app/src/main/java/com/realtimehitchhiker/hitchgo/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.realtimehitchhiker.hitchgo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.SeekBar;
@@ -8,8 +10,10 @@ import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private SharedPreferences sharedPref;
     private TextView txtShowRadius;
     private SeekBar barRadius;
+    private int radius;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +24,26 @@ public class SettingsActivity extends AppCompatActivity {
         //Intent intent = getIntent();
         //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
+        sharedPref = this.getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        int defaultValue = getResources().getInteger(R.integer.pref_radius_min);
+        radius = sharedPref.getInt(getString(R.string.pref_radius), defaultValue);
+
         txtShowRadius = (TextView) findViewById(R.id.textView_prefs_radius_unit);
         barRadius = (SeekBar) findViewById(R.id.seekBar_prefs_radius);
+
+        txtShowRadius.setText(radius + R.string.pref_radius_unit);
+        barRadius.setProgress(radius);
 
         barRadius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                radius = barRadius.getProgress();
+                txtShowRadius.setText(radius + R.string.pref_radius_unit);
 
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt(getString(R.string.pref_radius), radius);
+                editor.commit();
             }
 
             @Override
